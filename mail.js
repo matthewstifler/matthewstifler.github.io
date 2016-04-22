07846d8fb85a87a1df61cd88cd27ea62b65ff3f2
matthewstifler <mtwstflr@gmail.com>
2016-04-22 03:37:09 +0300

    upload from c9
    22.04 3:40

---
+++ mail.js
@@ -0,0 +1,5 @@
+// JavaScript File
+var username = "mtwstflr";
+var hostname = "gmail.com";
+var linktext = username + "@" + hostname ;
+document.write("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
