f6fe754988367bcfcb7168c61955efc8797e86f2
matthewstifler <mtwstflr@gmail.com>
2016-04-22 14:32:29 +0300

    1st try scroll fix
    1selector to three selectors, comment typo

--- scroll.js
+++ scroll.js
@@ -1,5 +1,19 @@
-//firest try, simper one
-$('#nav-button').click(function(){
+//first try, simper one
+$('#nav-button-1').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+$('#nav-button-2').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+$('#nav-button-3').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
