1be4ef552b99ffa189f098ae2867105cc4233103
matthewstifler <mtwstflr@gmail.com>
2016-04-22 14:41:02 +0300

    1st try scroll - fix 2
    wrong jump position,
    stopping ongoing animation

--- scroll.js
+++ scroll.js
@@ -1,20 +1,20 @@
 //first try, simper one
 $('#nav-button-1').click(function(){
-    $('html, body').animate({
+    $('html, body').stop().animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
 });
 
 $('#nav-button-2').click(function(){
-    $('html, body').animate({
+    $('html, body').stop().animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
 });
 
 $('#nav-button-3').click(function(){
-    $('html, body').animate({
+    $('html, body').stop().animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
