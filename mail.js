0568af09f1515c8f65d11e5b5db723101c6468ac
Mikhail Ulitskiy <mtwstflr@gmail.com>
2017-06-06 17:23:34 +0300

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,5 +1,5 @@
 // JavaScript File
-var username = "mishaulitskiy";
+var username = "misha.ulitskiy";
 var hostname = "gmail.com";
 var linktext = username + "@" + hostname ;
 document.write("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
